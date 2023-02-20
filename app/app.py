b9f397bd7194fea473506368ddd2a1e3d82cd9e2
Daniel Soares <danielsoares.data@outlook.com>
2023-02-20 12:01:41 -0300

    Adding comments and rearranging the code

--- app/app.py
+++ app/app.py
@@ -2,24 +2,41 @@
 import streamlit as st
 import pandas as pd
 
-#%%
+# %%
 # Importing the data
 cleaned_data = pd.read_csv('../data/interim/all_players_data_withoutDuplicates.csv', index_col=0)
 similarity_df = pd.read_csv('../similarity_results/similarity_df.csv', index_col=0)
-#%%
+# %%
+# Getting the team and Player name
 team = st.sidebar.selectbox('Pick a team:', options=cleaned_data.Squad.drop_duplicates())
 player_name = st.sidebar.selectbox('Select a player:',
                                    options=cleaned_data.query(f"Squad == '{team}'").Player.drop_duplicates())
-rec_qtd = st.sidebar.selectbox('Choose the number of recommendations', options=(5, 10, 15, 20, 25, 30))
-st.sidebar.write(":warning: The greater the number of desired recommendations, the lower the accuracy of the recommendations.")
-#%%
-id_player = cleaned_data.query(f"Player == '{player_name}' & Squad == '{team}'").id.values[0]
-indexes = similarity_df.loc[id_player].sort_values(ascending=False).index.to_list()
-values = similarity_df.loc[id_player].sort_values(ascending=False).values
-data_to_show = cleaned_data.query(f"id == {indexes[1:int(rec_qtd)+1]}")[['Player', 'Age', 'Pos', 'Squad']]
-data_to_show['Similarity'] = values[1:rec_qtd+1]
-st.header("Find similar players")
-st.subheader(f'Players similar to {player_name}:')
-st.write(data_to_show.sort_values(by=['Similarity', 'Age'], ascending=False))
-#%%
 
+# Getting the amount of recommendations
+rec_qtd = st.sidebar.selectbox('Choose the number of recommendations:', options=(5, 10, 15, 20, 25, 30))
+
+# Creating the sidebar
+st.sidebar.write(":warning: The further down the table, the more precise the recommendation is.")
+# %%
+# Fetching the player id
+id_player = cleaned_data.query(f"Player == '{player_name}' & Squad == '{team}'").id.values[0]
+
+# Searching players similar to him in the similarity dataset
+indexes = similarity_df.loc[id_player].sort_values(ascending=False).index.to_list()
+
+# Removing the player himself from the dice
+del indexes[0]
+
+# Getting player position and filtering similar players by position
+position = cleaned_data.query(f"id == '{id_player}'").Pos.values[0]
+data_to_show = cleaned_data.query(f"id == {indexes}")[['Player', 'Age', 'Pos', 'Squad']]
+data_to_show = data_to_show.query(f"Pos == '{position}'")
+
+# Writing the page header and sub header
+st.header("Player recommendation system")
+st.subheader(f'Players similar to {player_name}:')
+
+# Showing the table with the recommendations
+data_to_show = data_to_show.reset_index().drop(columns='index')
+st.write(data_to_show.head(rec_qtd))
+# %%
